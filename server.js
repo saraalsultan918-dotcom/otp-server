13ea2325ef9dc5c84ffacf853da36280a4f20115
Bedoor <saraalsultan918@gmail.com>
2026-04-29 22:08:58 +0300

    fix firebase env

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 
-const admin = require("firebase-admin");
-const serviceAccount = JSON.parse(
-  process.env.FIREBASE_KEY.replace(/\\n/g, '\n')
-);
 
+const admin = require("firebase-admin");
+
+const serviceAccount = {
+  type: "service_account",
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
